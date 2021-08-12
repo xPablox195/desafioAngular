44bd21c2d68ca7c53b8a4a34a76bb8c0c7d69dbf
pluparia <lupariapablo@gmail.com>
2021-08-12 18:19:28 -0300

    Delete header component y modal component
    Se eliminaron componentes no implementados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { PackagedateService } from './services/packagedate.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -20,7 +19,6 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import { ModalcommentsComponent } from './components/modalcomments/modalcomments.component';
 import { FormularioComponent } from './pages/formulario/formulario.component';
 
 import { FormsModule } from '@angular/forms';
@@ -32,11 +30,9 @@ import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     ListadoComponent,
     NavbarComponent,
     HomeComponent,
-    ModalcommentsComponent,
     FormularioComponent
   ],
   imports: [
